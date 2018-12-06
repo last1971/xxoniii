4d16356bb1bd2e6adc867fc84827be45dbf47d98
vladimir <elcopro@gmail.com>
2018-12-06 20:27:43 +0300

    Киномакс rigth

--- database/seeds/TheaterSeeder.php
+++ database/seeds/TheaterSeeder.php
@@ -15,6 +15,7 @@ class TheaterSeeder extends Seeder
         \App\Theater::updateOrCreate(['id' => '6440'], ['name' => 'Горизонт', 'kinoplan' => true]);
         \App\Theater::updateOrCreate(['id' => '6665'], ['name' => 'Кинополис Парк', 'kinoplan' => true]);
         \App\Theater::updateOrCreate(['id' => '3963'], ['name' => 'Кинополис Орбита', 'kinoplan' => true]);
+        \App\Theater::updateOrCreate(['id' => '321'], ['name' => 'Дом Кино', 'kinoplan' => true]);
         \App\Theater::updateOrCreate(
             ['id' => 'https://kinomax.ru/rostov-imax/'],
             ['name' => 'Киномакс-IMAX Ростов-На-Дону', 'kinoplan' => false]
